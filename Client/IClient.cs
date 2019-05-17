d314789f086d7ab899bb39ef556c95f6b94a2e37
Mysliwietz Florian <myslflo1996@yahoo.de>
2019-05-17 10:29:38 +0200

    feat(client): Add IClient interface definition

---
+++ Client/IClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client
+{
+    interface IClient
+    {
+        /// <summary>
+        /// On setup an uid for a service is defined. Therefore, one client
+        /// will only connect to a single service. The reason for this behavior
+        /// is the ability, to request (add a view) periodically.
+        /// </summary>
+        /// <param name="uid"></param>
+        void Setup(string uid);
+
+        /// <summary>
+        /// A request to the Gateway is initiated that will cause the service
+        /// defined in the setup to increase the number of views.
+        /// </summary>
+        void AddViews(int number = 1);
+
+        /// <summary>
+        /// With this methods the client can add periodically view to a service.
+        /// The number of views added and the time between requests can be set.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="intervalInSeconds"></param>
+        void StartPeriodicRequests(int number = 1, int intervalInSeconds = 5);
+
+        /// <summary>
+        /// To stop periodic requests this method can be called.
+        /// </summary>
+        void StopPeriodicRequests();
+    }
+}
