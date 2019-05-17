f07f8fd185c1fa3db713bc7c948a1184fb542114
Mysliwietz Florian <myslflo1996@yahoo.de>
2019-05-17 09:13:15 +0200

    chore(setup): Add initial project stubs

---
+++ Cheetah/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cheetah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
