551f5a219a0a516a0801882382e1eac56b4f57fa
Mysliwietz Florian <myslflo1996@yahoo.de>
2019-05-17 11:47:10 +0200

    feat(gateway): Add GatewayController
    - Modify launch settings for gateway ASP.NET application

---
+++ Gateway/Controllers/GatewayController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gateway.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class GatewayController : ControllerBase
+    {
+        [HttpGet("{service-uid}/views")]
+        public ActionResult<int> GetTotalViews(int serviceUid)
+        {
+            Console.WriteLine($"Get cumulated known views from {serviceUid}");
+            // TODO: get cumulated known views from service
+            return 0;
+        }
+
+        [HttpPost("{service-uid}/addView")]
+        public void AddView(int serviceUid)
+        {
+            Console.WriteLine($"Add view to {serviceUid}");
+            // TODO: add view to service with uid
+        }
+
+        [HttpPost("{service-uid}/addView/{number}")]
+        public void AddViews(int serviceUid, int number)
+        {
+            Console.WriteLine($"Add {number} views to {serviceUid}");
+            // TODO: add number of views to service with uid
+        }
+    }
+}
