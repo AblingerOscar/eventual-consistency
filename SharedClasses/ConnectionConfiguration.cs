18ef5a10cd769adf0d11fd1216ad3d81ff727abe
SourceOverflow <oscarablinger@gmail.com>
2019-05-24 12:53:24 +0200

    feat(shared): Add ConnectionConfiguration with the HostName

---
+++ SharedClasses/ConnectionConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedClasses
+{
+    public static class ConnectionConfiguration
+    {
+        public const string HostName = "https://localhost:5001/";
+    }
+}
