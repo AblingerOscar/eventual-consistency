1d37d44d6502d615f2c9c0e4099b72332f210524
SourceOverflow <oscarablinger@gmail.com>
2019-05-24 13:00:08 +0200

    feat(shared): Add NotSetupException to shared classes

---
+++ SharedClasses/NotSetupException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SharedClasses
+{
+    [Serializable]
+    public class NotSetupException : Exception
+    {
+        public NotSetupException()
+        {
+        }
+
+        public NotSetupException(string message) : base(message)
+        {
+        }
+
+        public NotSetupException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NotSetupException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
