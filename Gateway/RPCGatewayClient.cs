58cc295c8ce5288e280e6481e9c2782a838d7fd8
SourceOverflow <oscarablinger@gmail.com>
2019-05-24 21:21:34 +0200

    fix(rpc): Fix RPCGatewayClient's namespace

--- Gateway/RPCGatewayClient.cs
+++ Gateway/RPCGatewayClient.cs
@@ -6,7 +6,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 
-namespace ViewService
+namespace Gateway
 {
     public class RPCGatewayClient : IDisposable
     {
