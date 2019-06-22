8c911feba949c9670b37dc315f8df1a564541b6d
SourceOverflow <oscarablinger@gmail.com>
2019-06-23 01:58:22 +0200

    feat(heartbeat): Expand heartbeat data objects

--- SharedClasses/DataObjects/HeartbeatAnswer.cs
+++ SharedClasses/DataObjects/HeartbeatAnswer.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SharedClasses.DataObjects
 {
-    class HeartbeatAnswer
+    public class HeartbeatAnswer
     {
+        public string SenderUID { get; set; }
+        public Dictionary<string, DateTime> NewerChanges { get; set; }
+
+        public byte[] ToBytes()
+        {
+            var json = JsonConvert.SerializeObject(this);
+            return Encoding.UTF8.GetBytes(json);
+        }
+
+        public static HeartbeatAnswer FromBytes(byte[] bytes)
+        {
+            var json = Encoding.UTF8.GetString(bytes);
+            return JsonConvert.DeserializeObject<HeartbeatAnswer>(json);
+        }
     }
 }
--- SharedClasses/DataObjects/HeartbeatRequest.cs
+++ SharedClasses/DataObjects/HeartbeatRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,20 @@ namespace SharedClasses.DataObjects
 {
     public class HeartbeatRequest
     {
+        public string SenderUID;
+        public IDictionary<string, DateTime> KnownChanges
+            = new Dictionary<string, DateTime>();
 
+        public byte[] ToBytes()
+        {
+            var json = JsonConvert.SerializeObject(this);
+            return Encoding.UTF8.GetBytes(json);
+        }
+
+        public static HeartbeatRequest FromBytes(byte[] bytes)
+        {
+            var json = Encoding.UTF8.GetString(bytes);
+            return JsonConvert.DeserializeObject<HeartbeatRequest>(json);
+        }
     }
 }
