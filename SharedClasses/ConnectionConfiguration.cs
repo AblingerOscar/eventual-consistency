b4d7881e6f092b9f20bb3382456095d360fff611
SourceOverflow <oscarablinger@gmail.com>
2019-06-23 01:55:50 +0200

    fix(sync-service): Change host name constant to all upper cases

--- SharedClasses/ConnectionConfiguration.cs
+++ SharedClasses/ConnectionConfiguration.cs
@@ -6,7 +6,7 @@ namespace SharedClasses
 {
     public static class ConnectionConfiguration
     {
-        public const string HostName = "http://localhost:5001/";
+        public const string HOSTNAME = "http://localhost:5001/";
         public const string HEARTBEAT_SIGNAL_EXCHANGE_NAME = "heartbeat_signal";
         public const string HEARTBEAT_ANSWER_EXCHANGE_NAME = "heartbeat_answer";
     }
