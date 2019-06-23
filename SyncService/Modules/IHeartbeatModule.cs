eb2fd982fde3462730a90cad664bc46798db50f8
SourceOverflow <oscarablinger@gmail.com>
2019-06-23 02:45:21 +0200

    fix(heartbeat): Fix wrong Handler argument type

--- SyncService/Modules/IHeartbeatModule.cs
+++ SyncService/Modules/IHeartbeatModule.cs
@@ -1,7 +1,9 @@
-﻿namespace SyncService.Modules
+﻿using SyncService.Modules.Heartbeat;
+
+namespace SyncService.Modules
 {
     public delegate void HeartbeatAnswerReceivedHandler(IHeartbeatModule source, HeartbeatAnswerReceivedArgs args);
-    public delegate void OutdatedLocalChangesHandler(IHeartbeatModule source, OutdatedLocalChangesHandler args);
+    public delegate void OutdatedLocalChangesHandler(IHeartbeatModule source, OutdatedLocalChangesArgs args);
 
     public interface IHeartbeatModule: IModule
     {
